ad564975a8288c8dc81ffda56fd9ed45e2c4159f
Apple <apple@Apples-MacBook-Air.local>
2025-11-22 12:57:04 +0530

    updated project

--- frontend/myapp/src/components/PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/PrivateRoute.jsx
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
-
-export default function PrivateRoute({ children }) {
-  const isAuth = useSelector((s) => s.user.isAuthenticated);
-  return isAuth ? children : <Navigate to="/login" replace />;
-}
