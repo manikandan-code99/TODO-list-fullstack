6d77967348b4c6edc98a773d60db4372c0ab8ab4
manikandan-code99 <manikandancode99@gmail.com>
2025-12-05 12:08:53 +0530

    dfg

--- backend/project/project/settings.py
+++ backend/project/project/settings.py
@@ -171,10 +171,10 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES':(
     #     'rest_framework.permissions.IsAuthenticated'
     # ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-}
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework_simplejwt.authentication.JWTAuthentication',
+#     ),
+ }
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=2),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=5),
